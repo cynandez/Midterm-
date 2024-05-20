85553ab4b0256e5f78402de2869f68428017ee67
cynandez <143136801+cynandez@users.noreply.github.com>
2024-05-20 16:00:12 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,222 +5,40 @@
 
 /* 
  * File:   main.cpp
- * Name: Cynthia Hernandez
+ *Name: Cynthia Hernandez
  *
  * 
  */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-class PlayingCard {
-public:
-    PlayingCard(int value)
-        : value_(value)
-    {
-    }
+void calculateProbability() {
+    int trials = 1000000;
+    int successes = 0;
 
-    void show() const {
-        cout << valueToString(value_) << "\n";
-    }
-    int getValue() const {
-        return value_;
-    }
-private:
-    int value_;
+    for (int i = 0; i < trials; ++i) {
+        int num1 = rand() % 100 + 1;
+        int num2 = rand() % 100 + 1;
+        int num3 = rand() % 100 + 1;
+        int num4 = rand() % 100 + 1;
+        int num5 = rand() % 100 + 1;
 
-    string valueToString(int value) const {
-        switch (value) {
-        case 1:
-            return "Ace";
-        case 2:
-            return "Two";
-        case 3:
-            return "Three";
-        case 4:
-            return "Four";
-        case 5:
-            return "Five";
-        case 6:
-            return "Six";
-        case 7:
-            return "Seven";
-        case 8:
-            return "Eight";
-        case 9:
-            return "Nine";
-        case 10:
-            return "Ten";
-        case 11:
-            return "Jack";
-        case 12:
-            return "Queen";
-        case 13:
-            return "King";
-        default:
-            return "";
-        }
-    }
-};
-
-class CardDeck {
-public:
-    CardDeck() {
-        for (int suit = 0; suit < 4; ++suit) {
-            for (int value = 1; value <= 13; ++value) {
-                if (value == 1 || value >= 10) { // create cards for Ace and face values
-                    deck_.emplace_back(value);
-                }
-            }
-        }
-    }
-    vector<PlayingCard> draw(int numCards) {
-        vector<PlayingCard> hand;
-        for (int i = 0; i < numCards; i++) {
-            hand.push_back(deck_.back());
-            deck_.pop_back();
-        }
-        return hand;
-    }
-
-    void shuffleDeck() {
-        random_shuffle(deck_.begin(), deck_.end());
-    }
-
-    void display() const {
-        for (const auto& card : deck_) {
-            card.show();
+        if (num1 <= 40 && num2 <= 40 && num3 <= 40 && num4 <= 40 && num5 <= 40) {
+            ++successes;
         }
     }
 
-private:
-    vector<PlayingCard> deck_;
-};
-
-double calculatePairProbability(int simulations) {
-    int pairCount = 0;
-
-    for (int i = 0; i < simulations; i++) {
-        CardDeck deck;
-        deck.shuffleDeck();
-        vector<PlayingCard> hand = deck.draw(4);
-        
-        for (size_t j = 0; j < hand.size(); j++) {
-            for (size_t k = j + 1; k < hand.size(); k++) {
-                if (hand[j].getValue() == hand[k].getValue()) {
-                    pairCount++;
-                    break;
-                }
-            }
-        }
-    }
-
-    double probability = static_cast<double>(pairCount) / (simulations * (4 * 3 / 2));
-    return probability;
-}
-
-double calculateTwoPairProbability(int simulations) {
-    int twoPairCount = 0;
-    for (int i = 0; i < simulations; ++i) {
-        CardDeck deck;
-        deck.shuffleDeck();
-
-        vector<PlayingCard> hand;
-        for (int j = 0; j < 4; ++j) {
-            hand.push_back(deck.draw(1)[0]);
-        }
-
-        int pairCount = 0;
-        for (int j = 0; j < hand.size(); ++j) {
-            for (int k = j + 1; k < hand.size(); ++k) {
-                if (hand[j].getValue() == hand[k].getValue()) {
-                    ++pairCount;
-                }
-            }
-        }
-        if (pairCount == 2) {
-            ++twoPairCount;
-        }
-    }
-    double noTwoPairProb = static_cast<double>(simulations - twoPairCount) / simulations;
-    return 1.0 - noTwoPairProb;
-}
-
-double calculateThreeOfAKindProbability(int simulations) {
-    int threeOfAKindCount = 0;
-
-    for (int i = 0; i < simulations; ++i) {
-        CardDeck deck;
-        deck.shuffleDeck();
-
-        vector<PlayingCard> hand;
-        for (int j = 0; j < 4; ++j) {
-            hand = deck.draw(3);
-        }
-
-        bool hasThreeOfAKind = false;
-        for (int j = 0; j < hand.size(); ++j) {
-            int count = 0;
-            for (int k = 0; k < hand.size(); ++k) {
-                if (hand[j].getValue() == hand[k].getValue()) {
-                    ++count;
-                }
-            }
-            if (count == 3) {
-                hasThreeOfAKind = true;
-                break;
-            }
-        }
-        if (hasThreeOfAKind) {
-            ++threeOfAKindCount;
-        }
-    }
-    return static_cast<double>(threeOfAKindCount) / simulations;
-}
-
-double calculateFourOfAKindProbability(int simulations) {
-    int fourOfAKindCount = 0;
-    for (int i = 0; i < simulations; ++i) {
-        CardDeck deck;
-        deck.shuffleDeck();
-
-        vector<PlayingCard> hand;
-        for (int j = 0; j < 4; ++j) {
-            hand = deck.draw(3);
-        }
-
-        int matchCount = 1;
-        for (int j = 0; j < hand.size(); ++j) {
-            for (int k = j + 1; k < hand.size(); ++k) {
-                if (hand[j].getValue() == hand[k].getValue()) {
-                    ++matchCount;
-                }
-            }
-        }
-        if (matchCount == 4) {
-            ++fourOfAKindCount;
-        }
-    }
-    return static_cast<double>(fourOfAKindCount) / simulations;
+    double probability = static_cast<double>(successes) / trials;
+    cout << "Chance of all numbers being within 40%: " << probability << endl;
 }
 
 int main() {
-    CardDeck deck;
-    deck.shuffleDeck();
-    vector<PlayingCard> hand = deck.draw(4);
-
-    int simulations = 100000;
-    double pairProbability = calculatePairProbability(simulations) * 100;
-    double twoPairProbability = calculateTwoPairProbability(simulations) * 100;
-    double threeOfAKindProbability = calculateThreeOfAKindProbability(simulations) * 100;
-    double fourOfAKindProbability = calculateFourOfAKindProbability(simulations) * 100;
-    
-    cout << "Probability of getting a pair: " << pairProbability << "%" << endl;
-    cout << "Probability of getting 2 pairs: " << twoPairProbability << "%" << endl;
-    cout << "Probability of getting three of a kind: " << threeOfAKindProbability << "%" << endl;
-    cout << "Probability of getting four of a kind: " << fourOfAKindProbability << "%" << endl;
+    srand(static_cast<unsigned>(time(0)));  // Seed the random number generator
+    calculateProbability();
     return 0;
 }
+
